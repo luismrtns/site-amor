499bfdb173653b4c2a683a486442258cdd8bab00
Luís Martins <137302218+luismrtns@users.noreply.github.com>
2023-09-04 16:12:38 -0300

    só umas anotações

--- texto.js
+++ texto.js
@@ -2,10 +2,10 @@ const texto = document.querySelector('#texto')
 
 function digitador(elemento){
     const textoarray = elemento.innerHTML.split('') // vai separar cada letra da frase
-    elemento.innerHTML = ''
-    textoarray.forEach((letra, indice) => {
-        setTimeout(() =>
-            elemento.innerHTML += letra, 70 * indice)
+    elemento.innerHTML = '' //vai tirar o texto da tela e só vai aparecer quando o site reiniciar
+    textoarray.forEach((letra, indice) => { //para cada letra no indice...
+        setTimeout(() => // adicionar a fatia de tempo para cada letra aparecer
+            elemento.innerHTML += letra, 70 * indice) // vai adicionar cada letra seguida da outra após 70 milisegundos
     });
     
 }
